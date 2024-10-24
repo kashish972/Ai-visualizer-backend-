b05c0a2cb95c0f08cc35131452c20d8004946b58
Kashish Arora <kashisharorasahib@gmail.com>
2024-10-24 16:46:28 +0530

    Added existing files to main repository

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+import imgAPI_Router from './routes/imgAPI.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.use('/api/img', imgAPI_Router);
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Hello from DALL.E" });
+});
+
+app.listen(8080, () => console.log('Server has started on port 8080'));
